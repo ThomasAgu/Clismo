52a88ceccb5554c6f55ef113e46f3caee2d7a09e
Thomas <thomas40651@gmail.com>
2023-06-17 11:36:46 -0300

    preview navbar responsive

--- pages/components/NavBar.jsx
+++ pages/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 //style
@@ -13,10 +14,16 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
 
+  const [active, setActive] = useState(false);
+
   const handleClickLogout = () =>{
     //aca sacar la sesion si la iniciamos
     dispatch(logout())
     router.push('/')
+  }
+
+  const setActiveSubmenu = () =>{
+    setActive(active => !active)
   }
 
   const dispatch = useDispatch();
@@ -30,7 +37,16 @@ const NavBar = () => {
           <div className='d-flex justify-content-end align-items-end me-3' id={styles.navBarDiv}>
               <button id={styles.logoutBtn} className={styles.btn} onClick={handleClickLogout}>Cerrar Sesion</button>
           </div>
-          <button id={styles.togglebtn} ><FontAwesomeIcon icon={faBars} /></button>
+          <div>
+            <button id={styles.togglebtn} onClick={setActiveSubmenu}><FontAwesomeIcon icon={faBars} /></button>
+            {active ? 
+              <div id={styles.divNavResponsive}>
+                <button className={styles.btnResponsive}  onClick={handleClickLogout}>Cerrar Sesion</button>
+              </div>
+              :
+              <></>
+            }
+          </div>
         </div>
       )
       } 
@@ -41,7 +57,19 @@ const NavBar = () => {
               <button id={styles.loginBtn} className={styles.btn} onClick={() => router.push('/Login')}  tabIndex={2}>Iniciar Sesion</button>
               <button id={styles.logupBtn} className={styles.btn} onClick={() => router.push('/Register')}  tabIndex={3}>Registrarse</button>
           </div>
-          <button id={styles.togglebtn}><FontAwesomeIcon icon={faBars} /></button>
+          <div >
+            <button id={styles.togglebtn} onClick={setActiveSubmenu}><FontAwesomeIcon icon={faBars} /></button>
+            {active ? 
+              <div id={styles.divNavResponsive}>
+                <button className={styles.btnResponsive}  onClick={() => router.push('/Login')}  tabIndex={2}>Iniciar Sesion</button>
+                <button className={styles.btnResponsive}  onClick={() => router.push('/Register')}  tabIndex={3}>Registrarse</button>
+              </div>
+            : 
+              <></>
+            }
+            
+            
+          </div>
         </div>
       )
     }
